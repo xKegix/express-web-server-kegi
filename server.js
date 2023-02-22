2bdff9b2e83b6af3dcf247a68dcdea3beacf1ea1
keginold philip <xkegix@users.noreply.github.com>
2023-02-22 10:23:42 +0000

    done

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const app = express();
 
+const PORT = 3000;
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log('Running Server on Port:${PORT}');
+})
